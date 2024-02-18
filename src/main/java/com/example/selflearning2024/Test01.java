72908aa97455a5597df7986f58247ae53a210a10
miles <1290672153@qq.com>
2024-02-18 20:20:40 +0800

    测试提交

--- src/main/java/com/example/selflearning2024/Test01.java
+++ src/main/java/com/example/selflearning2024/Test01.java
@@ -9,4 +9,8 @@ package com.example.selflearning2024;
  * @Version 1.0
  */
 public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
