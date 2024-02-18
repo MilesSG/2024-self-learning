df1c3c2500c94bbf042d986a38f4b05dfed64580
miles <1290672153@qq.com>
2024-02-18 20:15:52 +0800

    Initial commit

---
+++ src/main/java/com/example/selflearning2024/Test01.java
@@ -0,0 +1,12 @@
+package com.example.selflearning2024;
+
+/**
+ * @Author Miles
+ * @Date 2024/2/18 20:11
+ * @PackageName:com.example.selflearning2024
+ * @ClassName: Test01
+ * @Description: No Pain, No Gain
+ * @Version 1.0
+ */
+public class Test01 {
+}
