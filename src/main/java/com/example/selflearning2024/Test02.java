57e2990b738994eda3b471f08260d77fb8cef3bd
miles <1290672153@qq.com>
2024-02-18 20:21:43 +0800

    测试提交

---
+++ src/main/java/com/example/selflearning2024/Test02.java
@@ -0,0 +1,15 @@
+package com.example.selflearning2024;
+
+/**
+ * @Author Miles
+ * @Date 2024/2/18 20:16
+ * @PackageName:com.example.selflearning2024
+ * @ClassName: Test02
+ * @Description: No Pain, No Gain
+ * @Version 1.0
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
