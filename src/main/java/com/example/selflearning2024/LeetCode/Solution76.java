ca1a9fb3d5b9d9adc91d4e3a5352ecee9482acff
miles <1290672153@qq.com>
2024-02-19 22:51:31 +0800

    LeetCode76 矩阵置零 重新温故知新提交

--- src/main/java/com/example/selflearning2024/LeetCode/Solution76.java
+++ src/main/java/com/example/selflearning2024/LeetCode/Solution76.java
@@ -12,12 +12,15 @@ public class Solution76 {
 
     public void setZeroes(int[][] matrix) {
 
-        // 步骤1: 标记需要置零的行和列
-        boolean[] row = new boolean[matrix.length];
-        boolean[] col = new boolean[matrix[0].length];
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+        // 步骤1: 标记需要置零的行和列
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
                     row[i] = col[j] = true;
                 }
@@ -25,8 +28,8 @@ public class Solution76 {
         }
 
         // 步骤2: 根据标记置零对应的行和列
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (row[i] || col[j]) {
                     matrix[i][j] = 0;
                 }
